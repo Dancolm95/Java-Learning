8712d4b592b1f236a7ae9db0947e4bd4fdbbf403
Daniel Colona <dancolm95@gmail.com>
2025-06-02 22:28:13 -0500

    extends abstract class anonymously

---
+++ poo_Avanzada/Tarea.java
@@ -0,0 +1,18 @@
+package poo_Avanzada;
+
+//Extender una clase abstracta anonimamente
+
+public abstract class Tarea {
+  abstract void ejecutar();
+
+  public static void main(String[] args) {
+    Tarea nuevTarea = new Tarea() {
+      @Override
+      void ejecutar() {
+        System.out.println("Ejecutando tarea sin nombre");
+      }
+    };
+
+    nuevTarea.ejecutar();
+  }
+}
