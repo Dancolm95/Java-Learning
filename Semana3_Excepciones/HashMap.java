2a8c2d79c94a18c1a7152d1c7b2a45556fa4595e
daniel colona <danielcolona@daniels-MacBook-Air.local>
2025-06-12 22:49:12 -0500

    learning HashMap

---
+++ Semana3_Excepciones/HashMap.java
@@ -0,0 +1,13 @@
+public class HashMap{
+  public static void main(String[] args) {
+    Set<String> newMap = new HashSet<> ();
+    newMap.add("Dante");
+    newMap.add("Kattya");
+    newMap.add("Daniel");
+    newMap.add("Dante");
+    System.out.println('')
+      for(String n: newMap) {
+        System.out.println(n);
+      }
+  }
+}
